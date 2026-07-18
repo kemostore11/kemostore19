9371a1c05e7bcd14c047faf2ae594de654ac1718
kemostore11 <kemjkarem@gmail.com>
2026-07-18 21:23:43 +0300

    Add files via upload

---
+++ Products.js
@@ -0,0 +1,25 @@
+/* ================= SEARCH DATA ================= */
+
+const searchData = [
+
+{
+    keywords:["watch","watches","clock","ساعة","ساعات","smart","ذكية"],
+    page:"watches.html"
+},
+
+{
+    keywords:["bag","bags","شنطة","شنط","handbag"],
+    page:"bags.html"
+},
+
+{
+    keywords:["wallet","wallets","محفظة","محافظ","card holder"],
+    page:"wallets.html"
+},
+
+{
+    keywords:["world cup","football","كأس العالم","كاس العالم","كرة","مباراة"],
+    page:"World Cup Products.html"
+}
+
+];
